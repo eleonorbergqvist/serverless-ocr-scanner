7a2514b6cbdd76a5f33418fcc90d517f54fa1843
eleonorbergqvist <eleonore295@gmail.com>
2019-05-12 13:31:06 +0200

    Add missing iam permissions

--- src/textract.js
+++ src/textract.js
@@ -4,8 +4,6 @@ const analyzeDocument = async (document) => {
   const client = new AWS.Textract();
   const params = {
     Document: {
-      //Bytes: new Buffer('...') || 'STRING_VALUE' [> Strings will be Base-64 encoded on your behalf <],
-      //Bytes: document,
       Bytes: new Buffer(document, 'base64'),
     },
     FeatureTypes: [
